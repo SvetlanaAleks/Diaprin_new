b7bd27524fc441a00bc828515cb660c64193868d
Svetlana Alekseenko <s.alekseenko@everad.com>
2019-01-16 15:38:41 +0200

    add problem block

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,1 +1,21 @@
-//Some JS
+const App = (function () {
+   "use strict";
+   return {
+      sliderProblemInit: function () {
+         $('.problem__slider').slick({
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            cssEase: "linear",
+            prevArrow: '<div class="arr arr--left"><i class="fico fico-arrow"></i></div>',
+            nextArrow: '<div class="arr arr--right"><i class="fico fico-arrow"></i></div>',
+            autoplay: false
+         });
+      },
+      init: function () {
+         App.sliderProblemInit();
+      }
+   };
+})();
