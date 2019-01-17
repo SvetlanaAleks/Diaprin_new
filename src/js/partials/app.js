f35cba7aaa7638dc2848824e3608eb109f0fb1d4
Svetlana Alekseenko <s.alekseenko@everad.com>
2019-01-17 11:56:41 +0200

    fixed slider

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -4,10 +4,10 @@ const App = (function () {
       sliderProblemInit: function () {
          $('.problem__slider').slick({
             dots: true,
-            infinite: true,
+            infinite: false,
             speed: 500,
             slidesToShow: 3,
-            slidesToScroll: 1,
+            slidesToScroll: 3,
             cssEase: "linear",
             prevArrow: '<div class="arr arr--left"><i class="fico fico-arrow"></i></div>',
             nextArrow: '<div class="arr arr--right"><i class="fico fico-arrow"></i></div>',
