9048b168dec5265056d7e51e821745b19890f9ac
Svetlana Alekseenko <s.alekseenko@everad.com>
2019-01-21 10:49:46 +0200

    fixed slider

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,52 +1,55 @@
-const App = (function () {
-   "use strict";
-   return {
-      sliderProblemInit: function () {
-         $('.problem__slider').slick({
-            dots: true,
-            infinite: false,
-            speed: 500,
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            cssEase: "linear",
-            prevArrow: '<div class="arr arr--left"><i class="fico fico-arrow"></i></div>',
-            nextArrow: '<div class="arr arr--right"><i class="fico fico-arrow"></i></div>',
+const App = (function() {
+  "use strict";
+  return {
+    sliderProblemInit: function() {
+      $(".problem__slider").slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        cssEase: "linear",
+        prevArrow:
+          '<div class="arr arr--left"><i class="fico fico-arrow"></i></div>',
+        nextArrow:
+          '<div class="arr arr--right"><i class="fico fico-arrow"></i></div>',
 
-            autoplay: false,
-            responsive: [{
-                  breakpoint: 1024,
-                  settings: {
-                     slidesToShow: 2,
-                     slidesToScroll: 1
-                  }
-               },
-               {
-                  breakpoint: 640,
-                  settings: {
-                     slidesToShow: 1,
-                     slidesToScroll: 1,
-                     dots: false
-                  }
-               }
-            ]
-         });
-      },
-      showTabs: function () {
-         const tabs = $('.js-tabs');
-         const comment = $('.comment');
-         tabs.click(function (e) {
-            const _this = $(this);
-            tabs.removeClass('name--active');
-            _this.addClass('name--active');
-            const href = _this.data('target');
-            const target = $(href);
-            comment.removeClass('comment--active');
-            target.addClass('comment--active');
-         });
-      },
-      init: function () {
-         App.sliderProblemInit();
-         App.showTabs();
-      }
-   };
+        autoplay: false,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 640,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              dots: false
+            }
+          }
+        ]
+      });
+    },
+    showTabs: function() {
+      const tabs = $(".js-tabs");
+      const comment = $(".comment");
+      tabs.click(function(e) {
+        const _this = $(this);
+        tabs.removeClass("name--active");
+        _this.addClass("name--active");
+        const href = _this.data("target");
+        const target = $(href);
+        comment.removeClass("comment--active");
+        target.addClass("comment--active");
+      });
+    },
+    init: function() {
+      App.sliderProblemInit();
+      App.showTabs();
+    }
+  };
 })();
